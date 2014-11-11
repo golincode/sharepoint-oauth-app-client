a79121262023e6c1d92ddd0ee1ed39433f644306
Quetzy Garcia <qgarcia@wearearchitect.com>
2014-11-11 16:00:00 +0000

    chore(*): rename getByName() to getByRelativeURL() as it makes more sense

--- src/SPFolder.php
+++ src/SPFolder.php
@@ -180,7 +180,7 @@ class SPFolder implements SPListInterface
 	}
 
 	/**
-	 * Get a SharePoint Folder by Name
+	 * Get a SharePoint Folder by Relative URL
 	 *
 	 * @static
 	 * @access  public
@@ -190,7 +190,7 @@ class SPFolder implements SPListInterface
 	 * @throws  SPException
 	 * @return  SPFolder
 	 */
-	public static function getByName(SPSite &$site, $relative_url = null, $fetch = false)
+	public static function getByRelativeURL(SPSite &$site, $relative_url = null, $fetch = false)
 	{
 		$name = basename($relative_url);
 
