23b6304e5f25335b88e2c8e76f7d6e70599f33b3
Quetzy Garcia <quetzyg@altek.org>
2014-11-15 22:42:11 +0000

    chore(SPSite): added initial tests

---
+++ tests/SPSiteTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * This file is part of the SharePoint OAuth App Client package.
+ *
+ * @author     Quetzy Garcia <qgarcia@wearearchitect.com>
+ * @copyright  2014 Architect 365
+ * @link       http://architect365.co.uk
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed
+ * with this source code.
+ */
+
+namespace WeAreArchitect\SharePoint;
+
+use GuzzleHttp\Client;
+use PHPUnit_Framework_TestCase;
+
+class SPSiteTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * Test SPSite constructor to FAIL (invalid URL)
+	 *
+	 * @expectedException         \WeAreArchitect\SharePoint\SPException
+	 * @expectedExceptionMessage  The SharePoint Site URL is invalid
+	 *
+	 * @access  public
+	 * @return  void
+	 */
+	public function testSPSiteConstructorFailInvalidURL()
+	{
+		$http = new Client();
+
+		$this->assertInstanceOf('\GuzzleHttp\Client', $http);
+
+		new SPSite($http, []);
+	}
+
+	/**
+	 * Test SPSite constructor to PASS
+	 *
+	 * @access  public
+	 * @return  SPSite
+	 */
+	public function testSPSiteConstructorPass()
+	{
+		$http = new Client([
+			'base_url' => 'https://example.sharepoint.com/sites/mySite/'
+		]);
+
+		$this->assertInstanceOf('\GuzzleHttp\Client', $http);
+
+		$site = new SPSite($http, []);
+
+		$this->assertInstanceOf('\WeAreArchitect\SharePoint\SPSite', $site);
+
+		return $site;
+	}
+
+	/**
+	 * Test SPSite getSPAccessToken() method to FAIL (invalid token)
+	 *
+	 * @depends                   testSPSiteConstructorPass
+	 * @expectedException         \WeAreArchitect\SharePoint\SPException
+	 * @expectedExceptionMessage  Invalid SharePoint Access Token
+	 *
+	 * @access  public
+	 * @param   SPSite  $site SharePoint Site
+	 * @return  void
+	 */
+	public function testSPSiteGetSPAccessTokenFailInvalidToken(SPSite $site = null)
+	{
+		$site->getSPAccessToken();
+	}
+}
