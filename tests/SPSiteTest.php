677fe8194028f1c4de9e4005b77ebcf01e147c17
Quetzy Garcia <quetzyg@altek.org>
2014-12-02 21:59:45 +0000

    chore(*): added tests/assertions

--- tests/SPSiteTest.php
+++ tests/SPSiteTest.php
@@ -14,6 +14,9 @@
 namespace WeAreArchitect\SharePoint;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Stream\Stream;
+use GuzzleHttp\Subscriber\Mock;
 use PHPUnit_Framework_TestCase;
 
 class SPSiteTest extends PHPUnit_Framework_TestCase
@@ -50,9 +53,35 @@ class SPSiteTest extends PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('\GuzzleHttp\Client', $http);
 
-        $site = new SPSite($http, []);
+        $responses = new Mock([
+            new Response(200, [], Stream::factory(json_encode([
+                'access_token' => 'iz%1&r<jVDoQJ74787#,Z4}4DQ8aw7',
+                'expires_on'   => 2147483647
+            ]))),
+
+            new Response(200, [], Stream::factory(json_encode([
+                'd' => [
+                    'GetContextWebInformation' => [
+                        'FormDigestValue'          => '1D98CAC834A6139426DF168F2E8ED',
+                        'FormDigestTimeoutSeconds' => 1800
+                    ]
+                ]
+            ])))
+        ]);
+
+        $http->getEmitter()->attach($responses);
+
+        $site = new SPSite($http, [
+            'resource'  => '00000000-0000-ffff-0000-000000000000/example.sharepoint.com@09g7c3b0-f0d4-416d-39a7-09671ab91f64',
+            'client_id' => '52848cad-bc13-4d69-a371-30deff17bb4d/example.com@09g7c3b0-f0d4-416d-39a7-09671ab91f64',
+            'secret'    => 'YzcZQ7N4lTeK5COin/nmNRG5kkL35gAW1scrum5mXVgE='
+        ]);
 
         $this->assertInstanceOf('\WeAreArchitect\SharePoint\SPSite', $site);
+
+        $this->assertEquals('/sites/mySite/', $site->getPath());
+        $this->assertEquals('https://example.sharepoint.com/', $site->getHostname());
+        $this->assertEquals('https://example.sharepoint.com/sites/mySite/', $site->getURL());
 
         return $site;
     }
@@ -71,5 +100,57 @@ class SPSiteTest extends PHPUnit_Framework_TestCase
     public function testSPSiteGetSPAccessTokenFailInvalidToken(SPSite $site = null)
     {
         $site->getSPAccessToken();
+    }
+
+    /**
+     * Test SPSite getSPAccessToken() method to PASS
+     *
+     * @depends testSPSiteConstructorPass
+     *
+     * @access  public
+     * @param   SPSite  $site SharePoint Site
+     * @return  void
+     */
+    public function testSPSiteGetSPAccessTokenPass(SPSite $site = null)
+    {
+        $site->createSPAccessToken();
+
+        $token = $site->getSPAccessToken();
+
+        $this->assertInstanceOf('\WeAreArchitect\SharePoint\SPAccessToken', $token);
+    }
+
+    /**
+     * Test SPSite getSPFormDigest() method to FAIL (invalid digest)
+     *
+     * @depends                   testSPSiteConstructorPass
+     * @expectedException         \WeAreArchitect\SharePoint\SPException
+     * @expectedExceptionMessage  Invalid SharePoint Form Digest
+     *
+     * @access  public
+     * @param   SPSite  $site SharePoint Site
+     * @return  void
+     */
+    public function testSPSiteGetSPFormDigestFailInvalidDigest(SPSite $site = null)
+    {
+        $site->getSPFormDigest();
+    }
+
+    /**
+     * Test SPSite getSPFormDigest() method to PASS
+     *
+     * @depends testSPSiteConstructorPass
+     *
+     * @access  public
+     * @param   SPSite  $site SharePoint Site
+     * @return  void
+     */
+    public function testSPSiteGetSPFormDigestPass(SPSite $site = null)
+    {
+        $site->createSPFormDigest();
+
+        $digest = $site->getSPFormDigest();
+
+        $this->assertInstanceOf('\WeAreArchitect\SharePoint\SPFormDigest', $digest);
     }
 }
