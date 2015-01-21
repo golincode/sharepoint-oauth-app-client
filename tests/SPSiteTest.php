57134bc5cf41bca57f62488fd687e19b37827614
Quetzy Garcia <qgarcia@wearearchitect.com>
2015-01-21 09:19:09 +0000

    chore(*): add testSPSiteGetSPAccessTokenWithContextPass() test

--- tests/SPSiteTest.php
+++ tests/SPSiteTest.php
@@ -17,6 +17,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Message\Response;
 use GuzzleHttp\Stream\Stream;
 use GuzzleHttp\Subscriber\Mock;
+use JWT;
 use PHPUnit_Framework_TestCase;
 
 class SPSiteTest extends PHPUnit_Framework_TestCase
@@ -54,20 +55,19 @@ class SPSiteTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\GuzzleHttp\Client', $http);
 
         $responses = new Mock([
+            // testSPSiteGetSPAccessTokenWithoutContextPass
             new Response(200, [], Stream::factory(json_encode([
                 'access_token' => 'iz%1&r<jVDoQJ74787#,Z4}4DQ8aw7',
                 'expires_on'   => 2147483647
             ]))),
 
+            // testSPSiteGetSPAccessTokenWithContextPass
             new Response(200, [], Stream::factory(json_encode([
-                'd' => [
-                    'GetContextWebInformation' => [
-                        'FormDigestValue'          => '1D98CAC834A6139426DF168F2E8ED',
-                        'FormDigestTimeoutSeconds' => 1800
-                    ]
-                ]
+                'access_token' => 'iz%1&r<jVDoQJ74787#,Z4}4DQ8aw7',
+                'expires_on'   => 2147483647
             ]))),
 
+            // testSPSiteGetSPAccessTokenWithContextPass
             new Response(200, [], Stream::factory(json_encode([
                 'd' => [
                     'GetContextWebInformation' => [
@@ -153,6 +153,39 @@ class SPSiteTest extends PHPUnit_Framework_TestCase
         $token = $site->getSPAccessToken();
 
         $this->assertInstanceOf('\WeAreArchitect\SharePoint\SPAccessToken', $token);
+    }
+
+    /**
+     * Test SPSite getSPAccessToken() method to PASS (with context)
+     *
+     * @depends testSPSiteConstructorPass
+     *
+     * @access  public
+     * @param   SPSite  $site SharePoint Site
+     * @return  void
+     */
+    public function testSPSiteGetSPAccessTokenWithContextPass(SPSite $site = null)
+    {
+        $data = [
+            'aud'                => '52848cad-bc13-4d69-a371-30deff17bb4d/example.com@09g7c3b0-f0d4-416d-39a7-09671ab91f64',
+            'iss'                => '00000000-0000-ffff-0000-000000000000@09g7c3b0-f0d4-416d-39a7-09671ab91f64',
+            'nbf'                => time(),
+            'exp'                => time()+1800,
+            'appctxsender'       => '00000000-0000-ffff-0000-000000000000@09g7c3b0-f0d4-416d-39a7-09671ab91f64',
+            'appctx'             => json_encode([
+                'CacheKey'                => '3+$xWJW69Xy+k5%KD=Tp6<NYT=8:qY{H31w7Q8a6+=xi5Jq8(<m6bGz.8S6f*0$',
+                'NextCacheKey'            => null,
+                'SecurityTokenServiceUri' => 'https://accounts.accesscontrol.windows.net/tokens/OAuth/2'
+            ]),
+            'refreshtoken'       => '73xXmf0RGc4YvH0VErnCstTH6X925QXC',
+            'isbrowserhostedapp' => true
+        ];
+
+        $access_token = JWT::encode($data, 'secret_key');
+
+        $site->createSPAccessToken($access_token);
+
+        $this->assertInstanceOf('\WeAreArchitect\SharePoint\SPAccessToken', $site->getSPAccessToken());
     }
 
     /**
