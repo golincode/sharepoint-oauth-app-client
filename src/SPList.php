f4b187fe5cf5c11104c79d4e4285c9e0615aab7f
Quetzy Garcia <qgarcia@wearearchitect.com>
2015-03-06 16:56:14 +0000

    chore(*): updated file headers

--- src/SPList.php
+++ src/SPList.php
@@ -7,7 +7,7 @@
  * @link       http://architect365.co.uk
  *
  * For the full copyright and license information,
- * please view the LICENSE file that was distributed
+ * please view the LICENSE.md file that was distributed
  * with this source code.
  */
 
